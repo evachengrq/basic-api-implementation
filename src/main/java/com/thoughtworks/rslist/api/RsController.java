6d2e09d9f7fafc9e78c93f64dabc9c1d5f797de5
程若前 <evachengrq@hodowakamaes-MacBook-Air.local>
2020-09-16 21:04:53 +0800

    [evacheng]feat: implemented add one rs event method

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -3,13 +3,20 @@ package com.thoughtworks.rslist.api;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class RsController {
-  private List<String> rsList = Arrays.asList("第一条事件", "第二条事件", "第三条事件");
-
+  private List<String> rsList = initRsList();
+  private List<String> initRsList() {
+    List<String> tempList = new ArrayList<>();
+    tempList.add("第一条事件");
+    tempList.add("第二条事件");
+    tempList.add("第三条事件");
+    return tempList;
+  }
 
   @GetMapping("/rs/{index}")
   public String getRsEvent(@PathVariable int index) {
@@ -23,5 +30,10 @@ public class RsController {
       return rsList.toString();
     }
     return rsList.subList(start - 1, end).toString();
+  }
+
+  @PostMapping("rs/event")
+  public void addRsEvent(@RequestBody String rsEventInput) {
+    rsList.add(rsEventInput);
   }
 }
